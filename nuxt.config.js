53f293f8aaaa6f3554b5a7a37448136a0e4cf3f0
Bakir Aitkulov <bakir.aitkul@gmail.com>
2019-10-30 15:31:41 +0600

    Remove onRequest interceptor, remove proxy in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,7 +66,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    proxy: true,
+    proxy: process.env.NODE_ENV !== 'production',
     prefix: '/api/v1'
   },
   proxy: {
