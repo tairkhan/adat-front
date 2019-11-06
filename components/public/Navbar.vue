aa07483fd96d447038954c500d1a8a1358fb3718
Tair <50920351+tairkhan@users.noreply.github.com>
2019-11-06 11:06:44 +0600

    Responsive navbar

--- components/public/Navbar.vue
+++ components/public/Navbar.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class="navbar flex flex-wrap justify-between items-center">
-    <div>
-      <nuxt-link class="navbar__link" :to="localePath('index')">
-        {{ $t('rubrics.main') }}
-      </nuxt-link>
-      <nuxt-link class="navbar__link" :to="localePath({ name: 'categories-slug' })">
-        {{ $t('rubrics.all') }}
-      </nuxt-link>
+  <div>
+    <span class="inline-block py-1 cursor-pointer lg:hidden" @click="navbarAction">
+      <fa-icon icon="bars" class="fa-fw" />
+    </span>
 
-      <nuxt-link
-        v-for="(item, i) in rubrics"
-        :key="i"
-        class="navbar__link"
-        :to="localePath({ name: 'categories-slug', params: { slug: item[$t('slug')] } })"
-      >
-        {{ item[$t('title')] }}
+    <div id="navbar" class="navbar">
+      <div class="flex flex-1 flex-col lg:flex-row lg:flex-wrap">
+        <nuxt-link class="navbar__link lg:mr-8" :to="localePath('index')">
+          {{ $t('rubrics.main') }}
+        </nuxt-link>
+
+        <nuxt-link class="navbar__link lg:mr-8" :to="localePath({ name: 'categories-slug' })">
+          {{ $t('rubrics.all') }}
+        </nuxt-link>
+
+        <nuxt-link
+          v-for="(item, i) in rubrics"
+          :key="i"
+          class="navbar__link lg:mr-8"
+          :to="localePath({ name: 'categories-slug', params: { slug: item[$t('slug')] } })"
+        >
+          {{ item[$t('title')] }}
+        </nuxt-link>
+      </div>
+
+      <nuxt-link class="my-2 py-1" :to="localePath('search')">
+        <fa-icon icon="search" class="fa-fw" />
       </nuxt-link>
     </div>
-
-    <nuxt-link class="mb-4 lg:m-0" :to="localePath('search')">
-      <span class="p-1 text-gray-800">
-        <fa-icon icon="search" class="fa-fw" />
-      </span>
-    </nuxt-link>
   </div>
 </template>
 
@@ -30,14 +35,27 @@
 import RubricMixin from '@/mixins/RubricMixin'
 
 export default {
-  mixins: [RubricMixin]
+  mixins: [RubricMixin],
+  methods: {
+    navbarAction () {
+      const navbar = document.getElementById('navbar')
+
+      if (navbar.className === 'navbar') {
+        navbar.className += ' responsive'
+      } else {
+        navbar.className = 'navbar'
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .navbar {
+  @apply flex-col hidden;
+
   &__link {
-    @apply relative inline-block py-4 mr-8 text-sm font-bold uppercase;
+    @apply relative py-4 text-sm font-bold uppercase;
 
     &::before {
       @apply absolute bottom-0 left-0 border-b border-2 border-red-600 w-full;
@@ -50,6 +68,16 @@ export default {
   .nuxt-link-exact-active::before {
     transform: scaleX(1);
     transition: .2s;
+  }
+}
+
+.responsive {
+  @apply flex;
+}
+
+@screen lg {
+  .navbar {
+    @apply flex flex-row justify-between items-center;
   }
 }
 </style>
